7e7cdac6d9fcf5edb5796b2b622904689829afb1
Hamza KAYA <hamza@hamzakaya.net>
2020-09-03 15:56:13 +0300

    fix

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const PORT = process.env.PORT || 3000
 app.get('/', async (_, res) => {
 
   const html = await axios({
-		method: 'get',
-		url: 'https://www.qnbfinansbank.enpara.com/hesaplar/doviz-ve-altin-kurlari'
+    method: 'get',
+    url: 'https://www.qnbfinansbank.enpara.com/hesaplar/doviz-ve-altin-kurlari'
   });
-  const getColumns = (x,y) => parseFloat(htmlParser.parse(html.data).querySelector('div.enpara-gold-exchange-rates__table').childNodes[x].childNodes[y].structuredText.match('\\d+,\\d+')[0].replace(',', '.'))
+  const getColumns = (x, y) => parseFloat(htmlParser.parse(html.data).querySelector('div.enpara-gold-exchange-rates__table').childNodes[x].childNodes[y].structuredText.match('\\d+,\\d+')[0].replace(',', '.'))
 
-  
-	res.status(200).json({
+
+  res.status(200).json({
     alis: {
-      dolar: getColumns(3,2), 
-      euro: getColumns(5,2), 
-      altin: getColumns(7,2)
+      dolar: getColumns(3, 2),
+      euro: getColumns(5, 2),
+      altin: getColumns(7, 2)
     },
     satis: {
-      dolar: getColumns(3,4), 
-      euro: getColumns(5,4), 
-      altin: getColumns(7,4)
+      dolar: getColumns(3, 4),
+      euro: getColumns(5, 4),
+      altin: getColumns(7, 4)
     }
   });
 
